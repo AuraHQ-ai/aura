1aaa89f55e77c9e91b9fde5da4ad6dccd36fe84f
Joan Rodriguez <joan@realadvisor.com>
2026-03-04 19:54:38 +0000

    fix: add statement-breakpoint to migration 0029

--- drizzle/0029_add_credential_type.sql
+++ drizzle/0029_add_credential_type.sql
@@ -1,2 +1,3 @@
 ALTER TABLE credentials ADD COLUMN type TEXT NOT NULL DEFAULT 'token';
+--> statement-breakpoint
 ALTER TABLE credentials ADD CONSTRAINT credentials_type_check CHECK (type IN ('token', 'oauth_client'));
